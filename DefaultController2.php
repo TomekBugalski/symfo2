2c2bc4f205f13f1a5f4c6f76cfbc202c3e00c40a
TomekBugalski <tbugalski@qmail.com>
2012-11-02 18:10:40 +0100

    aaa

---
+++ DefaultController2.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace My\NovelBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/{slug}.html", name="show")
+     * @Template()
+     */
+    public function showAction($slug)
+    {
+        $slugs = array();
+
+        $indexFile = file('../data/00index.log');
+
+        foreach ($indexFile as $tmp) {
+            $e = explode('|', trim($tmp));
+            $slugs[] = $e[1];
+        }
+
+        if (!in_array($slug, $slugs)) {
+            throw $this->createNotFoundException('Podana strona nie istnieje!');
+        }
+
+        $contents = file('../data/' . $slug . '.txt');
+        $title = array_shift($contents);
+        $novel = array(
+            'title' => trim($title),
+            'contents' => $contents
+        );
+        return compact('novel');
+    }
+
+    /**
+     * @Template()
+     */
+    public function menuAction()
+    {
+        $menuData  = array();
+        $indexFile = file('../data/00index.log');
+        foreach ($indexFile as $tmp) {
+            $e = explode('|', trim($tmp));
+            $menuData[] = array(
+                'title' => $e[0],
+                'slug' =>  $e[1]
+            );
+        }
+        return compact('menuData');
+    }
+
+}
