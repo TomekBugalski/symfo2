5f3bd8c333ba1f2df3c28f4ae6586ad7267abe46
TomekBugalski <tbugalski@qmail.com>
2012-11-02 18:30:57 +0100

    blabla

--- DefaultController.php
+++ DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace My\AnimalsBundle\Controller;
+namespace My\NovelBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -9,21 +9,48 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/jaszczurka.html", name="_jaszczurka")
+     * @Route("/{slug}.html", name="show")
      * @Template()
      */
-    public function jaszczurkaAction()
+    public function showAction($slug = 'index')
     {
-        return array();
+        $slugs = array();
+
+        $indexFile = file('../data/00index.log');
+
+        foreach ($indexFile as $tmp) {
+            $e = explode('|', trim($tmp));
+            $slugs[] = $e[1];
+        }
+
+        if (!in_array($slug, $slugs)) {
+            throw $this->createNotFoundException('Podana strona nie istnieje!');
+        }
+
+        $contents = file('../data/' . $slug . '.txt');
+        $title = array_shift($contents);
+        $novel = array(
+            'title' => trim($title),
+            'contents' => $contents
+        );
+        return compact('novel');
     }
-    
+
     /**
-     * @Route("/zaba.html", name="_zaba")
      * @Template()
      */
-    public function zabaAction()
+    public function menuAction()
     {
-        return array();
+        $menuData  = array();
+        $indexFile = file('../data/00index.log');
+        foreach ($indexFile as $tmp) {
+            $e = explode('|', trim($tmp));
+            $menuData[] = array(
+                'title' => $e[0],
+                'slug' =>  $e[1]
+            );
+        }
+        return compact('menuData');
     }
 
 }
